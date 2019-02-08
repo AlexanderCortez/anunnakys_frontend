a177219b0c72789751b6a244941e4513f1138032
alexandercortez <joelalexander10@outlook.com>
2019-02-07 22:47:53 -0600

    removing line

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import axios from 'axios';
 
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import store from './store';
